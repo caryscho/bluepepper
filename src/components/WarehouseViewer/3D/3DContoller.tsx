ece737aa1ee1c4126debe323d9fc5b4a7fc69fe9
carys <carys@willog.io>
2026-01-02 13:56:48 +0900

    refactor: enhance Contoller and ThreeDContoller with improved button styling

--- src/components/WarehouseViewer/3D/3DContoller.tsx
+++ src/components/WarehouseViewer/3D/3DContoller.tsx
@@ -8,10 +8,10 @@ function ThreeDContoller({
   onToggleAddDeviceMode,
 }: ThreeDContollerProps) {
   return (
-    <div className="absolute right-8 bottom-8 z-10 p-2 border shadow-md">
-      <button onClick={onToggleAddDeviceMode}>
+    <div className="absolute right-8 bottom-8 z-10 p-2">
+        <button onClick={onToggleAddDeviceMode} className="px-4 py-2 text-white bg-blue-500 rounded-lg">
         {isAddDeviceMode ? "Cancel" : "Add Device"}
-      </button>
+        </button>
     </div>
   );
 }
--- src/components/WarehouseViewer/DimContoller.tsx
+++ src/components/WarehouseViewer/DimContoller.tsx
@@ -1,13 +1,18 @@
 interface ContollerProps {
-  is2D: boolean;
-  onToggleDimension: () => void;
+    is2D: boolean;
+    onToggleDimension: () => void;
 }
 export default function Contoller({ is2D, onToggleDimension }: ContollerProps) {
-  return (
-    <>
-      <div className="absolute top-8 left-8 z-10 p-2 border shadow-md">
-        <button onClick={onToggleDimension}>{is2D ? '3D' : '2D'}</button>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="absolute top-8 left-8 z-10 p-2">
+                <button
+                    onClick={onToggleDimension}
+                    className="px-4 py-2 text-white bg-blue-500 rounded-lg"
+                >
+                    {is2D ? "2D -> 3D" : "3D -> 2D"}
+                </button>
+            </div>
+        </>
+    );
 }
