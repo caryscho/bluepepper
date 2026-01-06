6ebe15103431371ea1ad278eb3cd9649282eb288
carys <carys@willog.io>
2026-01-06 15:46:00 +0900

    feat: update Layout

--- src/app/ui/Layout.tsx
+++ src/app/ui/Layout.tsx
@@ -1,12 +1,14 @@
 import { Outlet, NavLink } from "react-router-dom";
 import "../../App.css";
+import { SearchIcon } from "lucide-react";
 
 export default function Layout() {
     return (
-        <div className="app">
-            <header className="flex justify-between p-4 w-full text-black bg-gray-100">
-                <h1>Playground</h1>
-                <nav className="flex gap-1">
+        <div className="flex">
+            <header className="flex flex-col p-4 w-full text-black bg-[#1b1e26] w-[240px] shrink-0">
+                <h1 className="text-2xl font-bold text-white">Willog</h1>
+
+                <nav className="flex flex-col gap-1 mt-10">
                     <NavLink to="warehouse">Warehouse</NavLink>
                 </nav>
             </header>
