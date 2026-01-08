867ff6a4d6c31a0e0213e8efaf3763d2ee94c220
carys <carys@willog.io>
2026-01-08 17:34:35 +0900

    feat: add Heatmap component and integrate into Controls

---
+++ src/widgets/warehouse-viewer/ui/controls/Heatmap.tsx
@@ -0,0 +1,9 @@
+import { MapIcon } from "lucide-react";
+
+export default function Heatmap() {
+    return (
+        <button className="hover:bg-blue-200">
+            <MapIcon className="w-4 h-4" />
+        </button>
+    );
+}
--- src/widgets/warehouse-viewer/ui/controls/index.tsx
+++ src/widgets/warehouse-viewer/ui/controls/index.tsx
@@ -1,9 +1,11 @@
 import DimensionToggle from "./DimensionToggle";
 import CenterModel from "./CenterModel";
 import DeviceContoller from "./DeviceContoller";
+import Heatmap from "./Heatmap";
 
 interface ControlsProps {
     is2D: boolean;
+    isDimensionLoading: boolean;
     isAddDeviceMode: boolean;
     onToggleDimension: () => void;
     onToggleAddDeviceMode: () => void;
@@ -13,6 +15,7 @@ interface ControlsProps {
 
 const Controls = ({
     is2D,
+    isDimensionLoading,
     isAddDeviceMode,
     onToggleDimension,
     onToggleAddDeviceMode,
@@ -22,9 +25,11 @@ const Controls = ({
     return (
         <>
             <div className="flex absolute bottom-8 left-8 z-10 flex-col gap-2 p-2 text-xs text-black shadow-me">
+                <Heatmap />
                 <CenterModel onResetCamera={onResetCamera} />
                 <DimensionToggle
                     is2D={is2D}
+                    isDimensionLoading={isDimensionLoading}
                     onToggleDimension={onToggleDimension}
                 />
             </div>
